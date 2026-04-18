e0b3be6d46c2c605d748c1dcc05b72e9144dac61
Dany Ashraf <danyashraf12@gmail.com>
2026-04-19 01:59:05 +0200

    Make Style file

---
+++ bookly_app/lib/Core/Utilits/styles.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+abstract class Styles {
+  static const TextStyle textStyle18 = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w400,
+  );
+  static const TextStyle textStyle20 = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w400,
+  );
+  static const TextStyle textStyle14 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+  );
+
+  static const TextStyle textStyle30 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+  );
+}
