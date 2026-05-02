93685996d326c58a8e09cff2376b643f9af79143
Dany Ashraf <danyashraf12@gmail.com>
2026-05-02 17:44:46 +0300

    createServiceLocator

---
+++ bookly_app/lib/Core/utilities/ServiceLocator.dart
@@ -0,0 +1,13 @@
+import 'package:bookly_app/Core/utilities/api_services.dart';
+import 'package:bookly_app/Features/home/Model/Repo/imp_repo.dart';
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+
+final getIt = GetIt.instance;
+
+void setupServiceLocator() {
+  getIt.registerSingleton<ApiServices>(ApiServices(Dio()));
+  getIt.registerSingleton<ImplementationRepo>(
+    ImplementationRepo(getIt.get<Dio>()),
+  );
+}
